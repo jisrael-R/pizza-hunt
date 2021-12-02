b654823d70f2adf842cab29272e4acbc82935bba
Jorman <jorman.iisrael@me.com>
2021-12-01 22:30:37 -0500

    added comment model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
 
     getAllPizza(req, res){
         Pizza.find({})
+          .populate({
+            path:'comments',
+            select: '-__v'
+          })
+          .select( '-__v')
+          .sort({ _id: -1 })
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err =>{
             console.log(err);
@@ -11,17 +17,23 @@ const pizzaController = {
         });
     },
 
-    getPizzaById({ params }, res){
-        Pizza.findOne({ _id:params.id})
-        .then(dbPizzaData =>{
-            if(!dbPizzaData){
-                res.status(404).json({ message: 'No Pizza found with this id' }); return;
-            }
-            res.json(dbPizzaData);
+    getPizzaById({ params }, res) {
+      Pizza.findOne({ _id: params.id })
+        .populate({
+          path: 'comments',
+          select: '-__v'
+        })
+        .select('-__v')
+        .then(dbPizzaData => {
+          if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+          }
+          res.json(dbPizzaData);
         })
         .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+          console.log(err);
+          res.status(400).json(err);
         });
     },
 
